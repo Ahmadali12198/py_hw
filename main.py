85b55a49e35bffa331c036a5c4c5cf8e9bfb43ac
aiali786 <ali.ksu12198@gmail.com>
2019-02-13 08:33:07 +0300

    First Add

---
+++ main.py
@@ -0,0 +1,37 @@
+#Homework-1 of 'Pyhon is Easy' Course  Submitted by Abdullah Khan
+
+#Title of the Album
+Title = "Nasheed"
+
+#Name of the Song
+Name = "Mera Dil Badal Dey"
+
+#Name of the Singer
+Singer = "Junaid Jamshed"
+
+"""
+Release Date as the below format:
+Year
+Month 
+Day
+"""
+Year,Month,Day = "2015", "September", "2"
+
+#Song Duration in Mintues & Seconds
+Duration = 5.43
+Minutes = 5
+Seconds = 43
+
+#Print Summary
+print("-------Summary-------")
+print("The Title of the Album is " + Title + ", " + "the Name of the Song is " + Name + " & " + "the singer is " + Singer + ".")
+
+#Print Details
+print('')
+print("-------Details-------")
+print("Title: " + Title)
+print("Name: " + Name)
+print("Singer: " + Singer)
+print("Release Date: " + Day + "/" + Month + "/" + Year)
+print("Duration: ", Minutes, "Minutes & ", Seconds, "Seconds")
+print("Title: " + Title)
