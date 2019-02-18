4726cb1803b774eeea5cf7dce52de9a1d2ecc11f
aiali786 <ali.ksu12198@gmail.com>
2019-02-18 20:58:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,29 +9,68 @@ Name = "Mera Dil Badal Dey"
 #Name of the Singer
 Singer = "Junaid Jamshed"
 
+LatestRelease = False
+
 """
 Release Date as the below format:
 Year
-Month 
+Month
 Day
 """
 Year,Month,Day = "2015", "September", "2"
 
-#Song Duration in Mintues & Seconds
-Duration = 5.43
-Minutes = 5
-Seconds = 43
+# #Song Duration in Mintues & Seconds
+# Duration = 5.43
+# Minutes = 5
+# Seconds = 43
+#
+# #Print Summary
+# print("-------Summary-------")
+# print("The Title of the Album is " + Title + ", " + "the Name of the Song is " + Name + " & " + "the singer is " + Singer + ".")
+#
+# #Print Details
+# print('')
+# print("-------Details-------")
+# print("Title: " + Title)
+# print("Name: " + Name)
+# print("Singer: " + Singer)
+# print("Release Date: " + Day +"/" + Month + "/" + Year)
+# print("Duration: ", Minutes, "Minutes & ", Seconds, "Seconds")
+# print('MashaAllah')
 
-#Print Summary
-print("-------Summary-------")
-print("The Title of the Album is " + Title + ", " + "the Name of the Song is " + Name + " & " + "the singer is " + Singer + ".")
+#Returns the Song Title
+def STitle():
+    SongTitle = Title
+    return SongTitle
 
-#Print Details
-print('')
-print("-------Details-------")
-print("Title: " + Title)
-print("Name: " + Name)
-print("Singer: " + Singer)
-print("Release Date: " + Day + "/" + Month + "/" + Year)
-print("Duration: ", Minutes, "Minutes & ", Seconds, "Seconds")
-print("Title: " + Title)
+print(STitle())
+numTwo = 2
+
+def addOne():
+    numThree = numTwo + 1
+    print(numThree)
+    return numThree
+
+addOne()
+#Returns Name of the Song
+def Song():
+    Song = Name
+    print(Song)
+    return Song
+
+Song()
+
+#Release Year
+def SYear():
+    Saal = Year
+    print(Saal)
+    return Saal
+
+SYear()
+
+def Release():
+    NewSongYear=LatestRelease
+    return NewSongYear
+
+print("Is latest Song Available:",Release())
+
